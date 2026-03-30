202bfe9b0e6c435c42a9c37c5bebbe3f2eba0f55
MalakNassar03 <malak.nassar12003@gmail.com>
2026-03-31 01:20:34 +0300

    init

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 import java.time.LocalDate;
-import java.time.LocalTime;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Clinic clinic=new Clinic();
